90d92d8da506881157aff4854eacba7eb6d37787
komuW <komuw05@gmail.com>
2020-08-15 15:55:06 +0300

    a

--- ote_test.go
+++ ote_test.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"sort"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -98,22 +97,98 @@ func TestCli(t *testing.T) {
 }
 
 func TestRun(t *testing.T) {
-	modContents, err := ioutil.ReadFile(gomodFile)
-	if err != nil {
-		t.Fatal(err)
+
+	tt := []struct {
+		fp                      string
+		modFilePath             string
+		expectedModfile         []byte
+		expectedModifiedModfile []byte
+	}{
+		{
+			fp:          "testdata/mod1",
+			modFilePath: "testdata/mod1/go.mod",
+			expectedModfile: []byte(`module testdata/mod1
+
+go 1.14
+
+require (
+	github.com/Shopify/sarama v1.27.0
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-cmp v0.5.1
+	github.com/nats-io/nats-server/v2 v2.1.7 // indirect
+	github.com/nats-io/nats.go v1.10.0
+	github.com/stretchr/testify v1.6.1 // SomePriorComment
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
+`),
+			expectedModifiedModfile: []byte(`module testdata/mod1
+
+go 1.14
+
+require (
+	github.com/Shopify/sarama v1.27.0
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-cmp v0.5.1
+	github.com/nats-io/nats-server/v2 v2.1.7 // indirect
+	github.com/nats-io/nats.go v1.10.0
+	github.com/stretchr/testify v1.6.1 // test; SomePriorComment
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
+
+`),
+		},
+
+		{
+			fp:          "testdata/mod3",
+			modFilePath: "testdata/mod3/go.mod",
+			expectedModfile: []byte(`module testdata/mod3
+
+go 1.14
+
+// ote should remove the //test comment from go-cmp since it is also used in main.go
+// it should also add a //test comment to testify
+require (
+	github.com/google/go-cmp v0.5.1 // test
+	github.com/pkg/json v0.0.0-20200630040052-6ff993914616
+	github.com/stretchr/testify v1.3.0
+)
+`),
+			expectedModifiedModfile: []byte(`module testdata/mod3
+
+go 1.14
+
+// ote should remove the //test comment from go-cmp since it is also used in main.go
+// it should also add a //test comment to testify
+require (
+	github.com/google/go-cmp v0.5.1
+	github.com/pkg/json v0.0.0-20200630040052-6ff993914616
+	github.com/stretchr/testify v1.3.0 // test
+)
+
+`),
+		},
 	}
 
-	if strings.Contains(string(modContents), testComment) {
-		t.Errorf("%#+v contained %#+v which is unexpected", gomodFile, testComment)
+	for _, v := range tt {
+		originalMod, err := ioutil.ReadFile(v.modFilePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// NB: you can use `github.com/kylelemons/godebug/diff` to find out how to make two strings equal
+		// diff := diff.Diff(string(v.expectedModfile), string(originalMod))
+		if !cmp.Equal(originalMod, v.expectedModfile) {
+			t.Errorf("\ngot \n\t%#+v \nwanted \n\t%#+v", originalMod, v.expectedModfile)
+		}
+
+		readonly := true
+		oteMod := new(bytes.Buffer)
+		run(v.fp, oteMod, readonly)
+
+		if !cmp.Equal(oteMod.Bytes(), v.expectedModifiedModfile) {
+			t.Errorf("\ngot \n\t%#+v \nwanted \n\t%#+v", originalMod, v.expectedModifiedModfile)
+		}
 	}
-
-	fp := "testdata/mod1"
-	readonly := true
-	buf := new(bytes.Buffer)
-	run(fp, buf, readonly)
-
-	if !strings.Contains(buf.String(), testComment) {
-		t.Errorf("re-rendered %#+v did NOT contain %#+v which is unexpected", gomodFile, testComment)
-	}
-
 }
