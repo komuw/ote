43beb5712281385aad1dd67c013916e033cd3d9d
komuW <komuw05@gmail.com>
2021-05-01 18:26:35 +0300

    update

--- ote_test.go
+++ ote_test.go
@@ -212,7 +212,7 @@ go 1.16
 require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-cmp v0.5.5
-	github.com/komuw/kama v0.0.0-20201012123531-9c57efc1ae36
+	github.com/komuw/kama v0.0.2
 	github.com/nats-io/nats-server/v2 v2.1.8 // indirect
 	github.com/nats-io/nats.go v1.10.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
@@ -226,7 +226,7 @@ go 1.16
 require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-cmp v0.5.5 // test
-	github.com/komuw/kama v0.0.0-20201012123531-9c57efc1ae36 // test
+	github.com/komuw/kama v0.0.2 // test
 	github.com/nats-io/nats-server/v2 v2.1.8 // indirect
 	github.com/nats-io/nats.go v1.10.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
