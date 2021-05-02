a01abab34c6c9bbae69bebe364c2993cdff1edf8
komuW <komuw05@gmail.com>
2021-05-02 09:13:38 +0300

    m

--- import.go
+++ import.go
@@ -13,6 +13,12 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
+// This lists are got from https://github.com/golang/go/blob/master/src/go/build/syslist.go
+// They should be synced periodically
+const goosList = "aix android darwin dragonfly freebsd hurd illumos ios js linux nacl netbsd openbsd plan9 solaris windows zos "
+const goarchList = "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm "
+const cGo = "cgo"
+
 var stdLibPkgs = make(map[string]struct{})
 
 func loadStd() error {
@@ -95,12 +101,6 @@ func fetchImports(file string) ([]string, error) {
 
 	return impPaths, nil
 }
-
-// This lists are got from https://github.com/golang/go/blob/master/src/go/build/syslist.go
-// They should be synced periodically
-const goosList = "aix android darwin dragonfly freebsd hurd illumos ios js linux nacl netbsd openbsd plan9 solaris windows zos "
-const goarchList = "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm "
-const cGo = "cgo"
 
 //
 // Usage:
