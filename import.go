811202988bdd76ccc2b44f91814ea866b0bae258
komuW <komuw05@gmail.com>
2021-05-02 09:13:11 +0300

    m

--- import.go
+++ import.go
@@ -96,15 +96,24 @@ func fetchImports(file string) ([]string, error) {
 	return impPaths, nil
 }
 
+// This lists are got from https://github.com/golang/go/blob/master/src/go/build/syslist.go
+// They should be synced periodically
+const goosList = "aix android darwin dragonfly freebsd hurd illumos ios js linux nacl netbsd openbsd plan9 solaris windows zos "
+const goarchList = "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm "
+const cGo = "cgo"
+
 //
 // Usage:
 //     fetchModule("/Users/komuw/mystuff/ote/testdata/mod2/", "github.com/hashicorp/nomad/drivers/shared/executor")
 func fetchModule(root, importPath string) (string, error) {
+	buildFlags := (strings.Join(strings.Split(goosList, " "), ",") +
+		strings.Join(strings.Split(goarchList, " "), ",") +
+		cGo)
 	cfg := &packages.Config{
-		Mode:  packages.NeedModule,
-		Tests: false,
-		// BuildFlags: []string{fmt.Sprintf("-tags=%s", buildFlags)},
-		Dir: filepath.Dir(root),
+		Mode:       packages.NeedModule,
+		Tests:      false,
+		BuildFlags: []string{fmt.Sprintf("-tags=%s", buildFlags)},
+		Dir:        filepath.Dir(root),
 	}
 	pkgs, err := packages.Load(
 		cfg,
