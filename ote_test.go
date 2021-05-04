d9222a806fd785aa142240062f4b0cd096eeeab2
komuW <komuw05@gmail.com>
2021-05-05 00:23:16 +0300

    m

--- ote_test.go
+++ ote_test.go
@@ -18,7 +18,18 @@ func Test_run(t *testing.T) {
 			name:     "ote's own modfile",
 			fp:       ".",
 			readonly: true,
-			want:     `ss`,
+			want: `module github.com/komuw/ote
+
+go 1.16
+
+require (
+	github.com/frankban/quicktest v1.12.1 // test
+	golang.org/x/mod v0.4.2
+	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
+	golang.org/x/tools v0.1.0
+)
+
+`,
 		},
 		{
 			name:     "testdata/mod1",
