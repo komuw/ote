2b3870d9860b7185114f579ff09f397c0b011889
komuW <komuw05@gmail.com>
2021-05-05 00:26:58 +0300

    m

--- ote_test.go
+++ ote_test.go
@@ -109,6 +109,30 @@ require (
 
 `,
 		},
+
+		{
+			name:     "testdata/mod4",
+			fp:       "testdata/mod4",
+			readonly: true,
+			want: `module testdata/mod4
+
+go 1.16
+
+require (
+	github.com/alexedwards/scs/v2 v2.4.0
+	github.com/aws/aws-sdk-go v1.38.31
+	github.com/benweissmann/memongo v0.1.1 // test
+	github.com/go-kit/kit v0.10.0
+	github.com/ishidawataru/sctp v0.0.0-20210226210310-f2269e66cdee
+	github.com/ory/herodot v0.9.5
+	github.com/rs/zerolog v1.21.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/zeebo/errs/v2 v2.0.3
+	go.uber.org/zap v1.13.0
+)
+
+`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
