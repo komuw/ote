30d4384dc909e4841b4fabe4c184c67840fd9712
komuW <komuw05@gmail.com>
2021-05-05 00:24:57 +0300

    m

--- ote_test.go
+++ ote_test.go
@@ -31,6 +31,7 @@ require (
 
 `,
 		},
+
 		{
 			name:     "testdata/mod1",
 			fp:       "testdata/mod1",
@@ -53,6 +54,40 @@ require (
 	golang.org/x/net v0.0.0-20210501142056-aec3718b3fa0 // indirect
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887
 	rsc.io/quote v1.5.2
+)
+
+`,
+		},
+
+		{
+			name:     "testdata/mod2",
+			fp:       "testdata/mod2",
+			readonly: true,
+			want: `module testdata/mod2
+
+go 1.16
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/chromedp/chromedp v0.7.1
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/frankban/quicktest v1.10.2 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/hashicorp/go-hclog v0.14.1 // indirect
+	github.com/hashicorp/vault/api v1.1.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
+	github.com/shirou/gopsutil v3.21.4+incompatible // test
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // test
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
 
 `,
