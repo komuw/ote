bfbe9eaa7aa2792c7114da1e427f8b0051969bcc
komuW <komuw05@gmail.com>
2021-06-11 22:03:54 +0300

    g

--- ote_test.go
+++ ote_test.go
@@ -119,7 +119,6 @@ go 1.16
 require (
 	github.com/alexedwards/scs/v2 v2.4.0
 	github.com/aws/aws-sdk-go v1.38.31
-	github.com/benweissmann/memongo v0.1.1 // test
 	github.com/go-kit/kit v0.10.0
 	github.com/ishidawataru/sctp v0.0.0-20210226210310-f2269e66cdee
 	github.com/ory/herodot v0.9.5
@@ -128,6 +127,8 @@ require (
 	github.com/zeebo/errs/v2 v2.0.3
 	go.uber.org/zap v1.13.0
 )
+
+require github.com/benweissmann/memongo v0.1.1 // test
 
 `,
 		},
