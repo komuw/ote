a84e2509e7e0946905ee008495924e41a335e047
komuW <komuw05@gmail.com>
2021-06-11 22:02:45 +0300

    g

--- ote_test.go
+++ ote_test.go
@@ -71,15 +71,18 @@ require (
 	github.com/hashicorp/vault/api v1.1.0
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
-	github.com/shirou/gopsutil v3.21.4+incompatible // test
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
 	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // test
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
+
+require (
+	github.com/shirou/gopsutil v3.21.4+incompatible // test
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // test
 )
 
 `,
