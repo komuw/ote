43ec3a92da24137eaa6138662767e01102135692
komuW <komuw05@gmail.com>
2021-06-11 22:01:36 +0300

    strings

--- ote_test.go
+++ ote_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
@@ -32,15 +33,18 @@ replace github.com/shirou/gopsutil => github.com/hashicorp/gopsutil v2.18.13-0.2
 require (
 	github.com/LK4D4/joincontext v0.0.0-20171026170139-1724345da6d5
 	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
-	github.com/frankban/quicktest v1.12.1 // test
 	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/hashicorp/nomad v1.0.4 // The test comment should be removed by ote
 	github.com/pkg/errors v0.9.1
-	github.com/shirou/gopsutil v2.20.9+incompatible // test; PriorComment
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20210501142056-aec3718b3fa0 // indirect
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887
 	rsc.io/quote v1.5.2
+)
+
+require (
+	github.com/frankban/quicktest v1.12.1 // test
+	github.com/shirou/gopsutil v2.20.9+incompatible // test; PriorComment
 )
 
 `,
@@ -162,6 +166,13 @@ require (
 			}
 
 			got := w.String()
+
+			fmt.Println()
+			fmt.Println()
+			t.Log(got)
+			fmt.Println()
+			fmt.Println()
+
 			c.Assert(got, qt.Equals, tt.want)
 		})
 	}
