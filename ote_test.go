9fdb648b28df0933e71868bd7d0cbf919a215cec
komuW <komuw05@gmail.com>
2021-06-12 22:58:58 +0300

    strings

--- ote_test.go
+++ ote_test.go
@@ -162,6 +162,18 @@ require github.com/benweissmann/memongo v0.1.1 // test
 
 go 1.16
 
+require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/hashicorp/vault/api v1.1.0
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/shirou/gopsutil v3.21.5+incompatible // test
+	rsc.io/quote v1.5.2 // test
+)
+
 `,
 		},
 
