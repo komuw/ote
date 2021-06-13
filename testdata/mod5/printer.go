ad7e12d47b50616b1d0337b1a3dacac3f7e05d40
komuW <komuw05@gmail.com>
2021-06-13 20:04:00 +0300

    dont pass any build tags

--- testdata/mod5/printer.go
+++ testdata/mod5/printer.go
@@ -6,6 +6,7 @@ import (
 	// this import is shared in test files and non-test files
 	// thus it should not be rendered by `ote` with a `// test` comment.
 	// this position will only be held if `ote` is able to ignore the build tags in this file.
+	// the tags make it so that this file can't be built in macOS,linux or windows(the places where we run `ote` tests/ci)
 	"github.com/kr/pretty"
 )
 
