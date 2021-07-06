4a9d790ec90053681095f8f90da6967bfff66000
$MY_NAME <komuw05@gmail.com>
2021-07-06 14:41:22 +0300

    m

--- testdata/mod7/main.go
+++ testdata/mod7/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/DataDog/zstd"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 )
 
 func main() {
 	fmt.Println("mod7")
 	_ = collectors.NewBuildInfoCollector()
+	_ = zstd.BestCompression
 }
