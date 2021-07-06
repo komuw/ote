77aa1b53cfa73bf0f12b054a2abffa3612654623
$MY_NAME <komuw05@gmail.com>
2021-07-06 15:52:11 +0300

    add explanation for why we ignore some errors

--- import.go
+++ import.go
@@ -105,7 +105,23 @@ func fetchModule(root, importPath string) (string, error) {
 	pkg := pkgs[0]
 	if pkg.Module == nil {
 		if len(pkg.Errors) > 0 {
-			return "", fmt.Errorf("unable to find module for import %s : %s", importPath, pkg.Errors[0].Msg)
+			if pkg.Errors[0].Kind == packages.ListError && strings.Contains(pkg.Errors[0].Msg, "build constraints exclude all Go files") {
+				// If a package depends on an import that requires an explicit build tag,
+				// then, `packages.Load()` is going to fail. The only way to fix it would be to pass in
+				// the explicit build tag required by that dependency to `packages.Config.BuildFlags`
+				// However, we do not know in advance what the required build tags are. That's why the call
+				// to `packages.Load()/packages.Config` do not have any build tag specified.
+				//
+				// As an example, `/testdata/mod1/` depends on `golang.org/x/sys/windows`. That dependency
+				// has the build tag; `+build windows`: https://github.com/golang/sys/blob/0f9fa26af87c481a6877a4ca1330699ba9a30673/windows/aliases.go#L5-L8
+				// and thus `packages.Load()` fails with error;
+				// `build constraints exclude all Go files in /pkg/mod/golang.org/x/sys@v0.0.1/windows`
+				//
+				// We can always add more errors here as/when we discover them
+				return "", nil
+			} else {
+				return "", fmt.Errorf("unable to find module for import %s : %s", importPath, pkg.Errors[0].Msg)
+			}
 		} else {
 			// this can be raised if an import path is inside a file that has some build tag
 			// that ote didn't take into account.
